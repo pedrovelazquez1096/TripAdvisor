6bb9feb4c82eb02cc13de4efddbe977f7d6cbd7f
pvelazquez1096 <86578496+pvelazquez1096@users.noreply.github.com>
2022-03-09 19:09:00 -0600

    Stacks added

---
+++ app/navigations/Navigation.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import RestaurantsStack from "./RestaurantsStack";
+import FavoritesStack from "./FavoritesStack";
+import TopRestaurantsStack from "./TopRestaurantsStack";
+import SearchStack from "./SearchStack";
+import AccountStack from "./AccountStack";
+
+import Restaurants from "../screens/Restaurants";
+import Favorites from "../screens/Favorites";
+import TopRestaurants from "../screens/TopRestaurants"
+import Search from "../screens/Search"
+import Account from "../screens/Account"
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation(){
+    return(
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="restaurants" component={RestaurantsStack} options={{title: "Restaurantes"}}/>
+                <Tab.Screen name="favorites" component={FavoritesStack} options={{title: "Favoritos"}}/>
+                <Tab.Screen name="topRestaurants" component={TopRestaurantsStack} options={{title: "Top 5"}}/>
+                <Tab.Screen name="search" component={SearchStack} options={{title: "Buscar"}}/>
+                <Tab.Screen name="account" component={AccountStack} options={{title: "Cuenta"}}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
